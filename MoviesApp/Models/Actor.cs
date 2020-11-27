0ed97962d2f17ff910bd4f56257d62db03b1158b
bugattiloh <bugattiloh2000@gmail.com>
2020-11-27 18:51:19 +0300

    Первые шаги!

---
+++ MoviesApp/Models/Actor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Models
+{
+    public class Actor
+    {
+       public int Id { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(128)]
+        public string LastName { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public Actor()
+        {
+            //Id?
+            Name = "";
+            LastName = "";
+            //Birthday?
+
+        }
+    }
+}
