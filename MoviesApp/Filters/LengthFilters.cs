e40930364fa96b7956176a19c53432f4fb235592
bugattiloh <bugattiloh2000@gmail.com>
2020-12-09 16:12:41 +0300

    fix old errors and add filters for actors add these ages

---
+++ MoviesApp/Filters/LengthFilters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApp.Filters
+{
+    public class LengthFilters : ValidationAttribute
+    {
+
+        public LengthFilters()  {}
+
+        public string OurError() => "Must be more 4 letters!";
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var str = (string)value;
+            if (str.Length < 4)
+            {
+                return new ValidationResult(OurError());
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- MoviesApp/ViewModels/InputActorViewModel.cs
+++ MoviesApp/ViewModels/InputActorViewModel.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using MoviesApp.Filters;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace MoviesApp.ViewModels
 {
     public class InputActorViewModel
     {
+        [LengthFilters]
         public string Name { get; set; }
 
+        [LengthFilters]
         public string LastName { get; set; }
 
-
-
+        
         [DataType(DataType.Date)]
         public DateTime Birthdate { get; set; }
     }
